3b28e9f89196b98a01222d09be19223bf19fffcb
Shin-sibainu <shincode0712@gmail.com>
2023-11-08 10:46:33 +0900

    useTodos added

---
+++ app/hooks/useTodos.ts
@@ -0,0 +1,21 @@
+import useSWR from "swr";
+
+async function fetcher(key: string) {
+  return fetch(key).then((res) => res.json());
+}
+
+export const useTodos = () => {
+  const API_URL = "";
+
+  const { data, isLoading, error, mutate } = useSWR(
+    `${API_URL}/allTodos`,
+    fetcher
+  );
+
+  return {
+    todos: data,
+    isLoading,
+    error,
+    mutate,
+  };
+};
